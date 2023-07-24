82eff1c0d076bec4487052924b1c69c167a1c167
Andre Sernajoto <andre.sernajoto@gmail.com>
2023-07-23 21:29:59 -0300

    day one

--- PokemonApi/Program.cs
+++ PokemonApi/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RestSharp;
+
+namespace PokemonApi
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            const string baseUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest();
+
+            Console.Write("Deseja digitar o nome do Pokemon? (y/n): ");
+            char option = Console.ReadLine()[0];
+
+            if (option.ToString().ToLower() == "n")
+            {
+                request = new ("", Method.Get);
+            }
+            else
+            {
+                Console.Write("\nDigite o nome do Pokemon: ");
+                string pokemonName = Console.ReadLine();
+
+                request = new ($"{pokemonName.ToLower()}", Method.Get);
+            }
+
+            var response = await client.ExecuteAsync(request);
+
+            Console.WriteLine($"\n{response.Content}");
+        }
+    }
+}
